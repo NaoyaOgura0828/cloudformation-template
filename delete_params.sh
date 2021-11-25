617834725c7d1c8139b9d9ffa073c4b963895f0a
SyatyoAdmiral <naoya_ogura@icloud.com>
2021-11-25 20:09:43 +0900

    キャッシュクリア

---
+++ delete_params.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+. ./.env_vars
+
+if [ $# -ne 1 ]; then
+    echo "      $0 <ENV_TYPE(dev|stg|prod)>"
+    exit 1
+elif [ "$1" != "dev" -a "$1" != "stg" -a "$1" != "prod" ]; then
+    echo "      $0 <ENV_TYPE(dev|stg|prod)>"
+    exit 1
+fi
+
+cd `dirname $0`
+
+SYSTEM_NAME=Template
+ENV_TYPE=$1
+
+aws ssm delete-parameter \
+    --name  ${SYSTEM_NAME}-${ENV_TYPE}-db-user \
+    --profile ${SYSTEM_NAME}-${ENV_TYPE}
+
+aws ssm delete-parameter \
+    --name  ${SYSTEM_NAME}-${ENV_TYPE}-db-pass \
+    --profile ${SYSTEM_NAME}-${ENV_TYPE}
+
+aws ssm delete-parameter \
+    --name ${SYSTEM_NAME}-${ENV_TYPE}-dockerhub-user \
+    --profile ${SYSTEM_NAME}-${ENV_TYPE}
+
+aws ssm delete-parameter \
+    --name ${SYSTEM_NAME}-${ENV_TYPE}-dockerhub-pass \
+    --profile ${SYSTEM_NAME}-${ENV_TYPE}
+
+exit 0
