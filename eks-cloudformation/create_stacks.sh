4980b3664c452f50cf6a4baa8ed78d2667e2e803
NaoyaOgura0828 <naoya_ogura@icloud.com>
2024-01-02 23:04:58 +0900

    FIX: Sort out eks-cloudformation

---
+++ eks-cloudformation/create_stacks.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+cd $(dirname $0)
+
+# 各システム名定義
+SYSTEM_NAME_TEMPLATE=template
+
+# 各環境名定義
+ENV_TYPE_DEV=dev
+ENV_TYPE_STG=stg
+ENV_TYPE_PROD=prod
+
+# 各リージョン名定義
+REGION_NAME_TOKYO=tokyo
+REGION_NAME_OSAKA=osaka
+REGION_NAME_VIRGINIA=virginia
+
+# スタック 作成
+create_stack() {
+    SYSTEM_NAME=$1
+    ENV_TYPE=$2
+    REGION_NAME=$3
+    SERVICE_NAME=$4
+
+    # スタック 作成
+    aws cloudformation create-stack \
+        --stack-name ${SYSTEM_NAME}-${ENV_TYPE}-${SERVICE_NAME} \
+        --template-body file://./${SYSTEM_NAME}/${SERVICE_NAME}/${SERVICE_NAME}.yml \
+        --cli-input-json file://./${SYSTEM_NAME}/${SERVICE_NAME}/${ENV_TYPE}-${REGION_NAME}-parameters.json \
+        --profile ${SYSTEM_NAME}-${ENV_TYPE}-${REGION_NAME}
+
+    # StackCreateComplete 待機
+    aws cloudformation wait stack-create-complete \
+        --stack-name ${SYSTEM_NAME}-${ENV_TYPE}-${SERVICE_NAME} \
+        --profile ${SYSTEM_NAME}-${ENV_TYPE}-${REGION_NAME}
+}
+
+#####################################
+# 構築対象リソース
+#####################################
+# create_stack ${SYSTEM_NAME_TEMPLATE} ${ENV_TYPE_DEV} ${REGION_NAME_TOKYO} iam-flowlog
+# create_stack ${SYSTEM_NAME_TEMPLATE} ${ENV_TYPE_DEV} ${REGION_NAME_TOKYO} iam-eks
+# create_stack ${SYSTEM_NAME_TEMPLATE} ${ENV_TYPE_DEV} ${REGION_NAME_TOKYO} kms
+# create_stack ${SYSTEM_NAME_TEMPLATE} ${ENV_TYPE_DEV} ${REGION_NAME_TOKYO} network
+# create_stack ${SYSTEM_NAME_TEMPLATE} ${ENV_TYPE_DEV} ${REGION_NAME_TOKYO} network-flowlog
+# create_stack ${SYSTEM_NAME_TEMPLATE} ${ENV_TYPE_DEV} ${REGION_NAME_TOKYO} eks
+
+exit 0
---
+++ eks-cloudformation/delete_stacks.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+cd $(dirname $0)
+
+# 各システム名定義
+SYSTEM_NAME_TEMPLATE=template
+
+# 各環境名定義
+ENV_TYPE_DEV=dev
+ENV_TYPE_STG=stg
+ENV_TYPE_PROD=prod
+
+# 各リージョン名定義
+REGION_NAME_TOKYO=tokyo
+REGION_NAME_OSAKA=osaka
+REGION_NAME_VIRGINIA=virginia
+
+# ShellScript実行時確認処理
+echo "------------------------------------------------------------------------------------------------------------------------------------------------------"
+read -p "削除対象リソースで選択された、CloudFormationで構築されている全リソースを削除します。実行してよろしいですか？ (Y/n) " yn
+
+case ${yn} in
+[yY])
+    echo '削除を開始します。'
+
+    # スタック 削除
+    delete_stack () {
+        SYSTEM_NAME=$1
+        ENV_TYPE=$2
+        REGION_NAME=$3
+        SERVICE_NAME=$4
+
+        aws cloudformation delete-stack \
+            --stack-name ${SYSTEM_NAME}-${ENV_TYPE}-${SERVICE_NAME} \
+            --profile ${SYSTEM_NAME}-${ENV_TYPE}-${REGION_NAME}
+
+        aws cloudformation wait stack-delete-complete \
+            --stack-name ${SYSTEM_NAME}-${ENV_TYPE}-${SERVICE_NAME} \
+            --profile ${SYSTEM_NAME}-${ENV_TYPE}-${REGION_NAME}
+    }
+
+    #####################################
+    # 削除対象リソース
+    #####################################
+    # delete_stack ${SYSTEM_NAME_TEMPLATE} ${ENV_TYPE_DEV} ${REGION_NAME_TOKYO} eks
+    # delete_stack ${SYSTEM_NAME_TEMPLATE} ${ENV_TYPE_DEV} ${REGION_NAME_TOKYO} network-flowlog
+    # delete_stack ${SYSTEM_NAME_TEMPLATE} ${ENV_TYPE_DEV} ${REGION_NAME_TOKYO} network
+    # delete_stack ${SYSTEM_NAME_TEMPLATE} ${ENV_TYPE_DEV} ${REGION_NAME_TOKYO} kms
+    # delete_stack ${SYSTEM_NAME_TEMPLATE} ${ENV_TYPE_DEV} ${REGION_NAME_TOKYO} iam-eks
+    # delete_stack ${SYSTEM_NAME_TEMPLATE} ${ENV_TYPE_DEV} ${REGION_NAME_TOKYO} iam-flowlog
+    # delete_stack ${SYSTEM_NAME_TEMPLATE} ${ENV_TYPE_DEV} ${REGION_NAME_TOKYO} aaa
+    # delete_stack ${SYSTEM_NAME_TEMPLATE} ${ENV_TYPE_DEV} ${REGION_NAME_TOKYO} bbb
+
+    echo '削除が完了しました。'
+    ;;
+*)
+    # 中止
+    echo '中止しました。'
+    ;;
+esac
+
+exit 0
