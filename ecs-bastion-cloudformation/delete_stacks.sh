f669e285ea10c0f81698da765d8b2e61a628cfa5
NaoyaOgura0828 <naoya_ogura@icloud.com>
2024-01-08 19:48:18 +0900

    FIX: Add delete_record_from_bastion_domain

--- ecs-bastion-cloudformation/delete_stacks.sh
+++ ecs-bastion-cloudformation/delete_stacks.sh
@@ -39,6 +39,56 @@ case ${yn} in
             --profile ${SYSTEM_NAME}-${ENV_TYPE}-${REGION_NAME}
     }
 
+    # Delete Record from Bastion Domain
+    delete_record_from_bastion_domain() {
+        SYSTEM_NAME=$1
+        ENV_TYPE=$2
+        REGION_NAME=$3
+        RECORD_TYPE=$4
+
+        parent_naked_domain=$(jq -r '.Parameters[] | select(.ParameterKey == "ParentNakedDomain").ParameterValue' "./templates/route53-hostzone/${ENV_TYPE}-${REGION_NAME}-parameters.json")
+
+        bastion_domain_id=$(
+            aws route53 list-hosted-zones-by-name \
+                --dns-name bastion.${parent_naked_domain} \
+                --query "HostedZones[0].Id" \
+                --profile ${SYSTEM_NAME}-${ENV_TYPE}-${REGION_NAME} \
+                --output text |
+                awk -F'/' '{print $3}'
+        )
+
+        bastion_public_ip=$(
+            aws route53 list-resource-record-sets \
+                --hosted-zone-id ${bastion_domain_id} \
+                --query "ResourceRecordSets[?Type == '${RECORD_TYPE}' && Name == 'bastion.${parent_naked_domain}.'].ResourceRecords[0].Value" \
+                --profile ${SYSTEM_NAME}-${ENV_TYPE}-${REGION_NAME} \
+                --output text
+        )
+
+        aws route53 change-resource-record-sets \
+            --hosted-zone-id ${bastion_domain_id} \
+            --profile ${SYSTEM_NAME}-${ENV_TYPE}-${REGION_NAME} \
+            --change-batch \
+            "{
+                \"Changes\": [
+                    {
+                        \"Action\": \"DELETE\",
+                        \"ResourceRecordSet\": {
+                            \"Name\": \"bastion.${parent_naked_domain}\",
+                            \"Type\": \"${RECORD_TYPE}\",
+                            \"TTL\": 300,
+                            \"ResourceRecords\": [
+                                {
+                                    \"Value\": \"${bastion_public_ip}\"
+                                }
+                            ]
+                        }
+                    }
+                ]
+            }" > /dev/null
+
+    }
+
     #####################################
     # 削除対象リソース
     #####################################
@@ -50,6 +100,7 @@ case ${yn} in
     # delete_stack ${SYSTEM_NAME_TEMPLATE} ${ENV_TYPE_DEV} ${REGION_NAME_TOKYO} keypair
     # delete_stack ${SYSTEM_NAME_TEMPLATE} ${ENV_TYPE_DEV} ${REGION_NAME_TOKYO} iam-ecs
     # delete_stack ${SYSTEM_NAME_TEMPLATE} ${ENV_TYPE_DEV} ${REGION_NAME_TOKYO} iam-flowlog
+    # delete_record_from_bastion_domain ${SYSTEM_NAME_TEMPLATE} ${ENV_TYPE_DEV} ${REGION_NAME_TOKYO} A
     # delete_stack ${SYSTEM_NAME_TEMPLATE} ${ENV_TYPE_DEV} ${REGION_NAME_TOKYO} route53-hostzone
 
     echo '削除が完了しました。'
